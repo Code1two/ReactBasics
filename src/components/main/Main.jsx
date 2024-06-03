eb6bc232d779b69e0f79dbefe6cc9960a8044562
Code1two <jokas0617@gmail.com>
2024-06-04 00:18:13 +0200

    Reacto darbine aplinka

---
+++ src/components/main/Main.jsx
@@ -0,0 +1,25 @@
+import Article from "../article/Article"
+const Main = ()=>{
+    return(
+        <main>
+            <Article
+            title = 'Naujiena 1'
+            content = 'Puiki diena'
+            />
+            <Article
+            title = 'Naujiena 2'
+            content = 'Puiki Labai diena'
+            />
+            <Article
+            title = 'Naujiena 3'
+            content = 'Labai Puiki diena'
+            />
+            <Article
+            title = 'Naujiena 4'
+            content = 'Tikrai Puiki diena'
+            />
+        </main>
+    )
+}
+
+export default Main
